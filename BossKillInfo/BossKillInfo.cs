f81f50b07e7532f6749fc66f7c9019d0f4d79d20
Keyou <keyouz@outlook.com>
2024-12-21 21:30:22 +0700

    Initial

---
+++ BossKillInfo/BossKillInfo.cs
@@ -0,0 +1,75 @@
+﻿using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace BossKillInfo
+{
+    [ApiVersion(2, 1)]
+    public class BossKillInfoPlugin : TerrariaPlugin
+    {
+        public override string Name => "BossKillInfo";
+        public override string Author => "Keyou";
+        public override string Description => "Displays a message with boss kill information and players who hit the boss.";
+        public override Version Version => new Version(1, 0);
+
+        private Dictionary<int, Dictionary<string, int>> bossDamage;
+
+        public BossKillInfoPlugin(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.NpcStrike.Register(this, OnNpcStrike);
+            ServerApi.Hooks.NpcKilled.Register(this, OnNpcKilled);
+            bossDamage = new Dictionary<int, Dictionary<string, int>>();
+        }
+
+        private void OnNpcStrike(NpcStrikeEventArgs args)
+        {
+            if (args.Npc.boss)
+            {
+                if (!bossDamage.ContainsKey(args.Npc.whoAmI))
+                {
+                    bossDamage[args.Npc.whoAmI] = new Dictionary<string, int>();
+                }
+
+                string playerName = TShock.Players[args.Player.whoAmI].Name;
+                if (!bossDamage[args.Npc.whoAmI].ContainsKey(playerName))
+                {
+                    bossDamage[args.Npc.whoAmI][playerName] = 0;
+                }
+
+                bossDamage[args.Npc.whoAmI][playerName] += args.Damage;
+            }
+        }
+
+        private void OnNpcKilled(NpcKilledEventArgs args)
+        {
+            if (args.npc.boss && bossDamage.ContainsKey(args.npc.whoAmI))
+            {
+                var totalDamage = bossDamage[args.npc.whoAmI].Values.Sum();
+                var message = $"[[C/FF0000:{args.npc.FullName}]]";
+
+                foreach (var player in bossDamage[args.npc.whoAmI])
+                {
+                    var percentage = player.Value / (float)totalDamage * 100;
+                    message += $"\n- [C/00FF00:{player.Key}] damage: [C/FFFF00:{player.Value}] ([C/00FFFF:{percentage:F1}%])";
+                }
+
+                TSPlayer.All.SendMessage(message, Microsoft.Xna.Framework.Color.LimeGreen);
+                bossDamage.Remove(args.npc.whoAmI);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.NpcStrike.Deregister(this, OnNpcStrike);
+                ServerApi.Hooks.NpcKilled.Deregister(this, OnNpcKilled);
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+# BossKillInfo Plugin
+
+BossKillInfo is a TShock plugin for Terraria that displays a message with boss kill information and the players who contributed to the boss's defeat.
+
+## Features
+
+- Tracks damage dealt to bosses by each player.
+- Displays a message with the boss's name, each player's damage, and their contribution percentage when a boss is killed.
+
+## Installation
+
+1. Download the plugin DLL file.
+2. Place the DLL file in the `ServerPlugins` folder of your TShock server.
+3. Restart your server.
+
+## Usage
+
+The plugin automatically tracks boss fights and displays the information when a boss is killed. No additional configuration is required.
+
+## Example
+
+When a boss is killed, a message like the following will be displayed:
+
+```
+[BossName]
+- Player1 damage: 5000 (50.0%)
+- Player2 damage: 3000 (30.0%)
+- Player3 damage: 2000 (20.0%)
+```
+
+## Development
+
+To contribute to the development of this plugin:
+
+1. Clone the repository.
+2. Open the project in your preferred C# IDE.
+3. Make your changes and submit a pull request.
+
+## License
+
+This project is licensed under the [MIT](./LICENSE) License.
